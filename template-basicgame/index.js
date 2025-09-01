2a55019e6d3a312814de62f626a4922d01fcf29f
ChemGuy1611 <10227968+ChemGuy1611@users.noreply.github.com>
2025-09-01 14:10:13 -0400

    added basic game template

---
+++ template-basicgame/index.js
@@ -0,0 +1,300 @@
+/*///////////////////////////////////////////
+Name: XXX Vortex Extension
+Structure: Basic Game
+Author: ChemBoy1
+Version: 0.1.0
+Date: 2025-09-01
+///////////////////////////////////////////*/
+
+//Import libraries
+const { actions, fs, util, selectors, log } = require('vortex-api');
+const path = require('path');
+const template = require('string-template');
+
+//Specify all the information about the game
+const STEAMAPP_ID = "XXX";
+const EPICAPP_ID = "XXX";
+const GOGAPP_ID = "XXX";
+const XBOXAPP_ID = "XXX";
+const XBOXEXECNAME = "XXX";
+const GAME_ID = "XXX";
+const GAME_NAME = "XXX";
+const GAME_NAME_SHORT = "XXX";
+const EXEC = "XXX.exe";
+const EXEC_XBOX = 'gamelaunchhelper.exe';
+
+let GAME_PATH = null;
+let GAME_VERSION = '';
+let STAGING_FOLDER = '';
+let DOWNLOAD_FOLDER = '';
+
+const MOD_ID = `${GAME_ID}-mod`;
+const MOD_NAME = "Mod";
+const MOD_PATH = "Mods";
+const MOD_PATH_XBOX = "Mods";
+
+const MOD_PATH_DEFAULT = '.';
+
+const spec = {
+  "game": {
+    "id": GAME_ID,
+    "name": GAME_NAME,
+    "shortName": GAME_NAME_SHORT,
+    "executable": EXEC,
+    "logo": `${GAME_ID}.jpg`,
+    "mergeMods": true,
+    "requiresCleanup": true,
+    "modPath": MOD_PATH_DEFAULT,
+    "modPathIsRelative": true,
+    "requiredFiles": [
+      EXEC
+    ],
+    "details": {
+      "steamAppId": STEAMAPP_ID,
+      "gogAppId": GOGAPP_ID,
+      "epicAppId": EPICAPP_ID,
+      "xboxAppId": XBOXAPP_ID,
+      //"supportsSymlinks": false,
+    },
+    "environment": {
+      "SteamAPPId": STEAMAPP_ID,
+      "GogAPPId": GOGAPP_ID,
+      "EpicAPPId": EPICAPP_ID,
+      "XboxAPPId": XBOXAPP_ID,
+    }
+  },
+  "modTypes": [
+    {
+      "id": MOD_ID,
+      "name": MOD_NAME,
+      "priority": "high",
+      "targetPath": `{gamePath}\\${MOD_PATH}`
+    },
+  ],
+  "discovery": {
+    "ids": [
+      STEAMAPP_ID,
+      //EPICAPP_ID,
+      //GOGAPP_ID,
+      //XBOXAPP_ID,
+    ],
+    "names": []
+  }
+};
+
+//3rd party tools and launchers
+const tools = [
+  {
+    id: `${GAME_ID}-customlaunch`,
+    name: 'Custom Launch',
+    logo: 'exec.png',
+    executable: () => EXEC,
+    requiredFiles: [
+      EXEC,
+    ],
+    relative: true,
+    exclusive: true,
+    shell: true,
+    //defaultPrimary: true,
+    parameters: [],
+  }, //*/
+];
+
+// BASIC EXTENSION FUNCTIONS ///////////////////////////////////////////////////
+
+//Set mod type priorities
+function modTypePriority(priority) {
+  return {
+    high: 25,
+    low: 75,
+  }[priority];
+}
+
+//Replace folder path string placeholders with actual folder paths
+function pathPattern(api, game, pattern) {
+  var _a;
+  return template(pattern, {
+    gamePath: (_a = api.getState().settings.gameMode.discovered[game.id]) === null || _a === void 0 ? void 0 : _a.path,
+    documents: util.getVortexPath('documents'),
+    localAppData: util.getVortexPath('localAppData'),
+    appData: util.getVortexPath('appData'),
+  });
+}
+
+//Set the mod path for the game
+function makeGetModPath(api, gameSpec) {
+  return () => gameSpec.game.modPathIsRelative !== false
+    ? gameSpec.game.modPath || '.'
+    : pathPattern(api, gameSpec.game, gameSpec.game.modPath);
+}
+
+//Get mod path dynamically for different game versions
+function getModPath(gamePath) {
+  GAME_VERSION = setGameVersion(gamePath);
+  if (GAME_VERSION === 'xbox') {
+    return MOD_PATH_XBOX;
+  }
+  else {
+    return MOD_PATH;
+  }
+} //*/
+
+//Find game installation directory
+function makeFindGame(api, gameSpec) {
+  return () => util.GameStoreHelper.findByAppId(gameSpec.discovery.ids)
+    .then((game) => game.gamePath);
+}
+
+async function requiresLauncher(gamePath, store) {
+  /*if (store === 'xbox') {
+      return Promise.resolve({
+          launcher: 'xbox',
+          addInfo: {
+              appId: XBOXAPP_ID,
+              parameters: [{ appExecName: XBOXEXECNAME }],
+          },
+      });
+  } //*/
+  /*if (store === 'epic') {
+    return Promise.resolve({
+        launcher: 'epic',
+        addInfo: {
+            appId: EPICAPP_ID,
+        },
+    });
+  } //*/
+  /*if (store === 'steam') {
+    return Promise.resolve({
+        launcher: 'steam',
+    });
+  } //*/
+  return Promise.resolve(undefined);
+}
+
+//Get correct executable for game version
+function getExecutable(discoveryPath) {
+  const isCorrectExec = (exec) => {
+    try {
+      fs.statSync(path.join(discoveryPath, exec));
+      return true;
+    }
+    catch (err) {
+      return false;
+    }
+  };
+  if (isCorrectExec(EXEC_XBOX)) {
+    return EXEC_XBOX;
+  }
+  else {
+    return EXEC;
+  };
+}
+
+//Get correct game version
+function setGameVersion(gamePath) {
+  const isCorrectExec = (exec) => {
+    try {
+      fs.statSync(path.join(gamePath, exec));
+      return true;
+    }
+    catch (err) {
+      return false;
+    }
+  };
+  if (isCorrectExec(EXEC_XBOX)) {
+    GAME_VERSION = 'xbox';
+    return GAME_VERSION;
+  };
+  if (isCorrectExec(EXEC)) {
+    GAME_VERSION = 'default';
+    return GAME_VERSION;
+  };
+}
+
+const getDiscoveryPath = (api) => { //get the game's discovered path
+  const state = api.getState();
+  const discovery = util.getSafe(state, [`settings`, `gameMode`, `discovered`, GAME_ID], {});
+  return discovery === null || discovery === void 0 ? void 0 : discovery.path;
+};
+
+async function purge(api) { //useful to clear out mods prior to doing some action
+  return new Promise((resolve, reject) => api.events.emit('purge-mods', true, (err) => err ? reject(err) : resolve()));
+}
+async function deploy(api) { //useful to deploy mods after doing some action
+  return new Promise((resolve, reject) => api.events.emit('deploy-mods', (err) => err ? reject(err) : resolve()));
+}
+
+// MAIN FUNCTIONS ///////////////////////////////////////////////////////////////
+
+//Setup function
+async function setup(discovery, api, gameSpec) {
+  // SYNCHRONOUS CODE ////////////////////////////////////
+  const state = api.getState();
+  GAME_PATH = discovery.path;
+  GAME_VERSION = setGameVersion(GAME_PATH);
+  STAGING_FOLDER = selectors.installPathForGame(state, GAME_ID);
+  DOWNLOAD_FOLDER = selectors.downloadPathForGame(state, GAME_ID);
+  // ASYNC CODE //////////////////////////////////////////
+  return fs.ensureDirWritableAsync(path.join(GAME_PATH, MOD_PATH_DEFAULT));
+}
+
+//Let Vortex know about the game
+function applyGame(context, gameSpec) {
+  //register game
+  const game = {
+    ...gameSpec.game,
+    queryPath: makeFindGame(context.api, gameSpec),
+    executable: () => gameSpec.game.executable,
+    //executable: getExecutable,
+    queryModPath: makeGetModPath(context.api, gameSpec),
+    //queryModPath: getModPath,
+    requiresLauncher: requiresLauncher,
+    setup: async (discovery) => await setup(discovery, context.api, gameSpec),
+    supportedTools: tools,
+  };
+  context.registerGame(game);
+
+  //register mod types
+  (gameSpec.modTypes || []).forEach((type, idx) => {
+    context.registerModType(type.id, modTypePriority(type.priority) + idx, (gameId) => {
+      var _a;
+      return (gameId === gameSpec.game.id)
+        && !!((_a = context.api.getState().settings.gameMode.discovered[gameId]) === null || _a === void 0 ? void 0 : _a.path);
+    }, (game) => pathPattern(context.api, game, type.targetPath), () => Promise.resolve(false), { name: type.name });
+  });
+
+  //register mod installers
+  //context.registerInstaller(MOD_ID, 25, testMod, installMod);
+
+  //register actions
+  context.registerAction('mod-icons', 300, 'open-ext', {}, 'View Changelog', () => {
+    const openPath = path.join(__dirname, 'CHANGELOG.md');
+    util.opn(openPath).catch(() => null);
+    }, () => {
+      const state = context.api.getState();
+      const gameId = selectors.activeGameId(state);
+      return gameId === GAME_ID;
+  });
+  context.registerAction('mod-icons', 300, 'open-ext', {}, 'Open Downloads Folder', () => {
+    const openPath = DOWNLOAD_FOLDER;
+    util.opn(openPath).catch(() => null);
+  }, () => {
+    const state = context.api.getState();
+    const gameId = selectors.activeGameId(state);
+    return gameId === GAME_ID;
+  });
+}
+
+//main function
+function main(context) {
+  applyGame(context, spec);
+  context.once(() => { // put code here that should be run (once) when Vortex starts up
+
+  });
+  return true;
+}
+
+//export to Vortex
+module.exports = {
+  default: main,
+};
